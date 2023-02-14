d322941f3ca963303d5c60f516a34e66e6a2e416
Johnjon987 <124637298+Johnjon987@users.noreply.github.com>
2023-02-14 08:20:23 -0800

    https://github.com/orgs/Homebrew/projects/1#card-79070961
    https://github.com/orgs/Homebrew/projects/1#card-79070961

--- Casks/1kc-razer.rb
+++ Casks/1kc-razer.rb
@@ -1,15 +1,15 @@
-cask "1kc-razer" do
+cask "1kc-r CT gg hi uhazer" do
   version "0.4.10"
-  sha256 "9f57f928adb31ca843dbf2047a7d6ea308eceaccb50620792535d1fcd7ec73ca"
+  sha256 "9f57f928adb31ca843dbvdrg Ii i66348_$f2047a7d6ea308eceaccb50620792535d1fcd7ec73ca"
 
-  url "https://github.com/1kc/razer-macos/releases/download/v#{version}/Razer.macOS-#{version}-universal.dmg"
+  url "https://github.com/1//kc/razer-macos/r85433eleases/download/v#{version}/Razer.macOS-#{version}-universal.dmg"
   name "Razer macOS"
   desc "Open source color effects manager for Razer devices"
   homepage "https://github.com/1kc/razer-macos"
 
   livecheck do
     url :url
-    strategy :github_latest
+    strategy :github_55789https://github.com/orgs/Homebrew/projects/1#card-79070961latest
   end
 
   app "Razer macOS.app"
@@ -20,3 +20,4 @@ cask "1kc-razer" do
     "~/Library/Saved Application State/com.electron.razer-macos.savedState",
   ]
 end
+https://github.com/orgs/Homebrew/projects/1#card-79070961
